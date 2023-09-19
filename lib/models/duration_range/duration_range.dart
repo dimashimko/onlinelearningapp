c5fe56a52d2b52a164ae61099aad14682277d556
dimashimko <dimashimko7@gmail.com>
2023-09-19 09:45:19 +0300

    add extends Equatable to models

--- lib/models/duration_range/duration_range.dart
+++ lib/models/duration_range/duration_range.dart
@@ -1,28 +1,38 @@
-class DurationRange {
-  const DurationRange({
+import 'package:equatable/equatable.dart';
+
+class DurationRangeModel extends Equatable {
+  const DurationRangeModel({
     required this.min,
     required this.max,
     required this.isEnable,
   });
+
   final int min;
   final int max;
   final bool isEnable;
+
+  //
+  @override
+  List<Object?> get props => [
+        min,
+        max,
+        isEnable,
+      ];
 
   @override
   String toString() {
     return 'min: $min, max: $max, isEnable: $isEnable';
   }
 
-  DurationRange copyWith({
+  DurationRangeModel copyWith({
     int? min,
     int? max,
     bool? isEnable,
   }) {
-    return DurationRange(
+    return DurationRangeModel(
       min: min ?? this.min,
       max: max ?? this.max,
       isEnable: isEnable ?? this.isEnable,
     );
   }
-
 }
