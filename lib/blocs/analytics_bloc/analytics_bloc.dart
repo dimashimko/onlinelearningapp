7e39c18ff2bca36483ee27b5c089f137daf19c7d
Діма Шимко <dimashimko7@gmail.com>
2023-10-19 15:10:09 +0300

    refactoring analytic bloc

--- lib/blocs/analytics_bloc/analytics_bloc.dart
+++ lib/blocs/analytics_bloc/analytics_bloc.dart
@@ -7,71 +7,73 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:online_learning_app/models/course/course_model.dart';
 
 part 'analytics_event.dart';
+
 part 'analytics_state.dart';
 
 class AnalyticsBloc extends Bloc<AnalyticsEvent, AnalyticsState> {
+  final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
   AnalyticsBloc() : super(const AnalyticsState()) {
+    on<OnOpenMyCoursesPageEvent>(_onOpenMyCoursesPageEvent);
+    on<OnTestLogEvent>(_onTestLogEvent);
+    on<OnCourseSelectedEvent>(_onCourseSelectedEvent);
+    on<OnBottomBarEvent>(_onBottomBarEvent);
+  }
 
-    final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  void _onOpenMyCoursesPageEvent(
+    OnOpenMyCoursesPageEvent event,
+    Emitter<AnalyticsState> emit,
+  ) async {
+    await analytics.logEvent(
+      name: 'on_open_my_course_page',
+    );
+  }
 
+  void _onTestLogEvent(
+    OnTestLogEvent event,
+    Emitter<AnalyticsState> emit,
+  ) async {
+    DateTime dateTime = DateTime.now();
+    String dateTimeNow = dateTime.toString();
+    String name = 'platformName';
+    if (Platform.isAndroid) {
+      name = 'android_dateTimeNow';
+    } else if (Platform.isIOS) {
+      name = 'iOS_dateTimeNow';
+    }
 
-
-    on<OnCourseSelectedEvent>(
-      (event, emit) async {
-
-        await analytics.logEvent(
-          name: 'course_selected',
-          parameters: <String, dynamic>{
-            'uid': event.currentCourse?.uid,
-            'name': event.currentCourse?.name,
-            'author': event.currentCourse?.author,
-            'category': event.currentCourse?.category,
-          },
-        );
+    await analytics.logEvent(
+      name: name,
+      parameters: <String, dynamic>{
+        'dateTime': dateTimeNow,
       },
     );
+  }
 
-    on<OnBottomBarEvent>(
-      (event, emit) async {
-
-
-        await analytics.logEvent(
-          name: 'bottom_bar_event',
-          parameters: <String, dynamic>{
-            'bottomBarIndex': event.routeName,
-          },
-        );
+  void _onCourseSelectedEvent(
+    OnCourseSelectedEvent event,
+    Emitter<AnalyticsState> emit,
+  ) async {
+    await analytics.logEvent(
+      name: 'course_selected',
+      parameters: <String, dynamic>{
+        'uid': event.currentCourse?.uid,
+        'name': event.currentCourse?.name,
+        'author': event.currentCourse?.author,
+        'category': event.currentCourse?.category,
       },
     );
+  }
 
-    on<OnTestLogEvent>(
-      (event, emit) async {
-
-        DateTime dateTime = DateTime.now();
-        String dateTimeNow = dateTime.toString();
-        String name = 'platformName';
-        if (Platform.isAndroid) {
-          name = 'android_dateTimeNow';
-        } else if (Platform.isIOS) {
-          name = 'iOS_dateTimeNow';
-        }
-
-        await analytics.logEvent(
-          name: name,
-          parameters: <String, dynamic>{
-            'dateTime': dateTimeNow,
-          },
-        );
+  void _onBottomBarEvent(
+    OnBottomBarEvent event,
+    Emitter<AnalyticsState> emit,
+  ) async {
+    await analytics.logEvent(
+      name: 'bottom_bar_event',
+      parameters: <String, dynamic>{
+        'bottomBarIndex': event.routeName,
       },
     );
-
-    on<OnOpenMyCoursesPageEvent>(
-      (event, emit) async {
-        await analytics.logEvent(
-          name: 'on_open_my_course_page',
-        );
-      },
-    );
-
   }
 }
--- lib/blocs/analytics_bloc/analytics_state.dart
+++ lib/blocs/analytics_bloc/analytics_state.dart
@@ -1,24 +1,13 @@
 part of 'analytics_bloc.dart';
 
-
 @immutable
 class AnalyticsState extends Equatable {
-  const AnalyticsState({
-    this.counter = 0,
-  });
-
-  final int counter;
+  const AnalyticsState();
 
   @override
-  List<Object?> get props => [
-        counter,
-      ];
+  List<Object?> get props => [];
 
-  AnalyticsState copyWith({
-    int? counter,
-  }) {
-    return AnalyticsState(
-      counter: counter ?? this.counter,
-    );
+  AnalyticsState copyWith() {
+    return const AnalyticsState();
   }
 }
