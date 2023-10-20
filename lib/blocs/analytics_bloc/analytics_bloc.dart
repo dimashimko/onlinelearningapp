bb0d7a3a05ee4f7659168a8f2d2d364bc358ac7d
Діма Шимко <dimashimko7@gmail.com>
2023-10-20 09:47:14 +0300

    transferred the processing logic of analytics bloc to the analyticsService

--- lib/blocs/analytics_bloc/analytics_bloc.dart
+++ lib/blocs/analytics_bloc/analytics_bloc.dart
@@ -1,10 +1,9 @@
-import 'dart:io';
-
 import 'package:equatable/equatable.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:online_learning_app/models/course/course_model.dart';
+import 'package:online_learning_app/services/analytics_service.dart';
 
 part 'analytics_event.dart';
 
@@ -12,56 +11,31 @@ part 'analytics_state.dart';
 
 class AnalyticsBloc extends Bloc<AnalyticsEvent, AnalyticsState> {
   final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  final AnalyticsService analyticsService = AnalyticsService();
 
   AnalyticsBloc() : super(const AnalyticsState()) {
     on<OnOpenMyCoursesPageEvent>(_onOpenMyCoursesPageEvent);
-    on<OnTestLogEvent>(_onTestLogEvent);
     on<OnCourseSelectedEvent>(_onCourseSelectedEvent);
     on<OnBottomBarEvent>(_onBottomBarEvent);
+    on<OnTestLogEvent>(_onTestLogEvent);
   }
 
   void _onOpenMyCoursesPageEvent(
     OnOpenMyCoursesPageEvent event,
     Emitter<AnalyticsState> emit,
   ) async {
-    await analytics.logEvent(
-      name: 'on_open_my_course_page',
-    );
-  }
-
-  void _onTestLogEvent(
-    OnTestLogEvent event,
-    Emitter<AnalyticsState> emit,
-  ) async {
-    DateTime dateTime = DateTime.now();
-    String dateTimeNow = dateTime.toString();
-    String name = 'platformName';
-    if (Platform.isAndroid) {
-      name = 'android_dateTimeNow';
-    } else if (Platform.isIOS) {
-      name = 'iOS_dateTimeNow';
-    }
-
-    await analytics.logEvent(
-      name: name,
-      parameters: <String, dynamic>{
-        'dateTime': dateTimeNow,
-      },
-    );
+    analyticsService.sendOpenMyCoursePageEvent();
   }
 
   void _onCourseSelectedEvent(
     OnCourseSelectedEvent event,
     Emitter<AnalyticsState> emit,
   ) async {
-    await analytics.logEvent(
-      name: 'course_selected',
-      parameters: <String, dynamic>{
-        'uid': event.currentCourse?.uid,
-        'name': event.currentCourse?.name,
-        'author': event.currentCourse?.author,
-        'category': event.currentCourse?.category,
-      },
+    analyticsService.sendCourseSelectedEvent(
+      uid: event.currentCourse?.uid,
+      name: event.currentCourse?.name,
+      author: event.currentCourse?.author,
+      category: event.currentCourse?.category,
     );
   }
 
@@ -69,11 +43,15 @@ class AnalyticsBloc extends Bloc<AnalyticsEvent, AnalyticsState> {
     OnBottomBarEvent event,
     Emitter<AnalyticsState> emit,
   ) async {
-    await analytics.logEvent(
-      name: 'bottom_bar_event',
-      parameters: <String, dynamic>{
-        'bottomBarIndex': event.routeName,
-      },
+    analyticsService.sendBottomBarEvent(
+      routeName: event.routeName,
     );
+  }
+
+  void _onTestLogEvent(
+    OnTestLogEvent event,
+    Emitter<AnalyticsState> emit,
+  ) async {
+    analyticsService.sendTestEvent();
   }
 }
--- lib/services/analytics_service.dart
+++ lib/services/analytics_service.dart
@@ -1,8 +1,59 @@
+import 'dart:io';
+
 import 'package:firebase_analytics/firebase_analytics.dart';
 
 class AnalyticsService {
-  final FirebaseAnalytics _analytics = FirebaseAnalytics.instance;
+  final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
-  FirebaseAnalyticsObserver getAnalyticsObserver() =>
-      FirebaseAnalyticsObserver(analytics: _analytics);
+  void sendOpenMyCoursePageEvent() async {
+    await analytics.logEvent(
+      name: 'on_open_my_course_page',
+    );
+  }
+
+  void sendCourseSelectedEvent({
+    required String? uid,
+    required String? name,
+    required String? author,
+    required String? category,
+  }) async {
+    await analytics.logEvent(
+      name: 'course_selected',
+      parameters: <String, dynamic>{
+        'uid': uid,
+        'name': name,
+        'author': author,
+        'category': category,
+      },
+    );
+  }
+
+  void sendBottomBarEvent({
+    required String? routeName,
+  }) async {
+    await analytics.logEvent(
+      name: 'bottom_bar_event',
+      parameters: <String, dynamic>{
+        'bottomBarIndex': routeName,
+      },
+    );
+  }
+
+  void sendTestEvent() async {
+    DateTime dateTime = DateTime.now();
+    String dateTimeNow = dateTime.toString();
+    String name = 'platformName';
+    if (Platform.isAndroid) {
+      name = 'android_dateTimeNow';
+    } else if (Platform.isIOS) {
+      name = 'iOS_dateTimeNow';
+    }
+
+    await analytics.logEvent(
+      name: name,
+      parameters: <String, dynamic>{
+        'dateTime': dateTimeNow,
+      },
+    );
+  }
 }
